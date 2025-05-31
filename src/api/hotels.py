0b39f16765df5a16a27c1f62b2164811a3b3d79f
Dmitry Kuznetsov <dmitry.kuznetsov@softgamings.com>
2025-05-31 13:12:00 +0300

    filter by location, title

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,90 +1,85 @@
 from fastapi import Query, APIRouter, Body
+from sqlalchemy import insert, select
 
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker, engine
+from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 
 hotel_router = APIRouter(prefix="/hotels", tags=["Отели"])
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
-    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
-    {"id": 5, "title": "Москва", "name": "moscow"},
-    {"id": 6, "title": "Казань", "name": "kazan"},
-    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
-]
-
-
-@hotel_router.get("/hotels")
-def get_hotels(
+@hotel_router.get("")
+async def get_hotels(
         pagination: PaginationDep,
-        id: int | None = Query(None, description="Айдишник"),
         title: str | None = Query(None, description="Название отеля"),
+        location: str | None = Query(None, description="Location"),
 ):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    if pagination.page and pagination.per_page:
-        return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
-    return hotels_
+    per_page = pagination.per_page or 5
+    async with async_session_maker() as session:
+        query = select(HotelsOrm)
+        if location:
+            query = query.where(HotelsOrm.location.like(f"%{location}%"))
+        if title:
+            query = query.where(HotelsOrm.title.like(f"%{title}%"))
+        query = (query
+                 .limit(per_page)
+                 .offset(per_page * (pagination.page-1))
+                 )
+        result = await session.execute(query)
+
+        hotels = result.scalars().all()
+        # print(type(hotels), hotels)
+        return hotels
 
 
-@hotel_router.delete("/hotels/{hotel_id}")
+@hotel_router.delete("/{hotel_id}")
 def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+#    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     return {"status": "OK"}
 
 
-@hotel_router.post("/hotels")
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+@hotel_router.post("")
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {
         "summary": "Сочи",
         "value": {
-            "title": "Отель Сочи 5 звезд у моря",
-            "name": "sochi_u_morya",
+            "title": "Отель Rich 5 звезд",
+            "location": "Сочи , ул. Моря, 1",
         }
     },
     "2": {
         "summary": "Дубай",
         "value": {
-            "title": "Отель Дубай У фонтана",
-            "name": "dubai_fountain",
+            "title": "Отель У фонтана",
+            "location": "Дубай, ул. Шейха, 2",
         }
     }
 })
 ):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": hotel_data.title,
-        "name": hotel_data.name,
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {"status": "OK"}
 
 
-@hotel_router.put("/hotels/{hotel_id}")
+
+@hotel_router.put("/{hotel_id}")
 def replace_hotel(
         hotel_id: int,
         hotel_data: Hotel
 ):
-    global hotels
-    hotels = [{"id": hotel_id, "name": hotel_data.name, "title": hotel_data.title} if _["id"] == hotel_id else _ for _
-              in hotels]
+    # hotels = [{"id": hotel_id, "name": hotel_data.name, "title": hotel_data.title} if _["id"] == hotel_id else _ for _
+    #           in hotels]
     return {"status": "OK"}
 
 
-@hotel_router.patch("/hotels/{hotel_id}")
+@hotel_router.patch("/{hotel_id}")
 def update_hotel(
         hotel_id: int,
         hotel_data: HotelPATCH
 ):
-    global hotels
     for index, hotel in enumerate(hotels):
         if hotel["id"] != hotel_id:
             continue

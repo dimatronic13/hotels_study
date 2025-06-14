0f5a439f44e2db85628a225f099e35f2455f4118
Dmitry Kuznetsov <dmitry.kuznetsov@softgamings.com>
2025-06-15 02:18:19 +0300

    hotels api pages

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -21,15 +21,24 @@ class HotelsRepository(BaseRepository):
             title,
             limit,
             offset,
-    ):
+    ) -> list[Hotel]:
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
         hotels_ids_to_get = (
             select(RoomsOrm.hotel_id)
             .select_from(RoomsOrm)
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-            .group_by(RoomsOrm.hotel_id)
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+        if title:
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+        query = (
+            query
             .limit(limit)
             .offset(offset)
         )
+        result = await self.session.execute(query)
 
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get) )
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]

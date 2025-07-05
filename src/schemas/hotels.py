9c15d910fd730384d5ce16735cd61f91eed1c6ee
Dmitry Kuznetsov <dmitry.kuznetsov@softgamings.com>
2025-07-05 17:55:29 +0300

    Data mapper

--- src/schemas/hotels.py
+++ src/schemas/hotels.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, ConfigDict
 
 
 class HotelAdd(BaseModel):
     title: str
     location: str
 
+
 class Hotel(HotelAdd):
     id: int
 
-    model_config = ConfigDict(from_attributes=True)
 
 class HotelPatch(BaseModel):
     title: str | None = None
